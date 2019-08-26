cad38f0640beaf1b91731d2916cdd1dd82e48be4
Syed Shahin Ahmed <syedshahinahmed@Syeds-MacBook-Pro.local>
2019-08-26 17:54:18 +0100

    Title function added

--- app.py
+++ app.py
@@ -7,31 +7,36 @@ from flask import Flask, render_template, redirect, request, url_for
 #### Config #####
 app = Flask(__name__)
 
+# Mongo Config 
+app.config["MONGO_DBNAME"] = "marfever"
+app.config["MONGO_URI"] = ""
+
 ################
 #### Route #####
 @app.route('/')
 def index():
+
     return render_template("index.html")
 
 # About Route
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title='About')
 
 # Contact Route
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", title='Contact')
 
 # Add Superhero Route
 @app.route('/addsuperhero')
 def addsuperhero():
-    return render_template("addchar.html")
+    return render_template("addchar.html", title='Add Superhero')
 
 # Edit Superhero Route
 @app.route('/editsuperhero')
 def editsuperhero():
-    return render_template("editchar.html")
+    return render_template("editchar.html", title='Edit Superhero')
 
 
 
