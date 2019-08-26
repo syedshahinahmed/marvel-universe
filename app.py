9c36a724ad4a915998b7fbc6d5317a5422865c26
Syed Shahin Ahmed <syedshahinahmed@Syeds-MacBook-Pro.local>
2019-08-26 17:48:22 +0100

    All the template created and style added as well

--- app.py
+++ app.py
@@ -3,10 +3,9 @@
 from flask import Flask, render_template, redirect, request, url_for
 
 
-
 ################
 #### Config #####
-app =  Flask(__name__)
+app = Flask(__name__)
 
 ################
 #### Route #####
@@ -14,8 +13,31 @@ app =  Flask(__name__)
 def index():
     return render_template("index.html")
 
+# About Route
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+# Contact Route
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+# Add Superhero Route
+@app.route('/addsuperhero')
+def addsuperhero():
+    return render_template("addchar.html")
+
+# Edit Superhero Route
+@app.route('/editsuperhero')
+def editsuperhero():
+    return render_template("editchar.html")
+
+
+
 #####################
 #### Deployment #####
+
 
 if __name__ == "__main__":
     app.run(debug=True)
