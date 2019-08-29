20c1ad15bc0d2ed4780325f76c7985242a05e4e9
Syed Shahin Ahmed <syedshahinahmed@Syeds-MacBook-Pro.local>
2019-08-29 22:11:34 +0100

    Add superhero route linked

--- app.py
+++ app.py
@@ -2,12 +2,13 @@
 #### Imports ###
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 ################
 #### Config #####
 app = Flask(__name__)
 
-# Mongo Config 
+# Mongo Config
 app.config["MONGO_URI"] = "mongodb+srv://syedroot:Azasr00t@syedcluster-eyeen.mongodb.net/marfever?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
@@ -15,7 +16,7 @@ mongo = PyMongo(app)
 #### Route #####
 @app.route('/')
 def index():
-    return render_template("index.html", superheros=mongo.db.superhero.find())
+    return render_template("index.html",superheros=mongo.db.superhero.find())
 
 # About Route
 @app.route('/about')
@@ -31,7 +32,7 @@ def contact():
 # View Superhero Route
 @app.route('/viewsuperhero/<url_for>')
 def viewsuperhero(url_for):
-    marvel={}
+    marvel = {}
     return render_template("viewsuperhero.html", title='View Superhero', superheros=mongo.db.superhero.find())
 
 # Add Superhero Route
@@ -45,27 +46,36 @@ def insertsuoerhero():
     # Connecting To Superhero Database
     superheros = mongo.db.superhero
 
-    # Adding Form Data To Superhero Database 
+    # Adding Form Data To Superhero Database
     superheros.insert_one(request.form.to_dict())
-    
 
     return redirect(url_for('index'))
 
 # Edit Superhero Route
-@app.route('/editsuperhero')
+# @app.route('/editsuperhero/<superhero_id>')
+# def editsuperhero(superhero_id):
+#     new_superhero = mongo.db.superhero.find_one(
+#         {"_id": ObjectId(superhero_id)})
+#     all_group = mongo.db.fav_superhero.find()
+#     return render_template("editsuperhero.html", title='Edit Superhero', superheros=new_superhero)
+
+
+# Edit Superhero Route
+@app.route("/editsuperhero")
 def editsuperhero():
-    return render_template("editsuperhero.html", title='Edit Superhero')
+    return render_template('editsuperhero.html', title='Edit Superhero')
 
-# test 
-@app.route('/test')
-def test():
-    return render_template("test.html")
+# Update Superhero
 
+
+# test
+# @app.route('/test')
+# def test():
+#     return render_template("test.html")
 
 
 #####################
 #### Deployment #####
-
 
 if __name__ == "__main__":
     app.run(debug=True)
