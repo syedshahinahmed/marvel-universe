0ad19e7b411b75e1977cc2af97b83423542082b1
Syed Shahin Ahmed <41708385+syedshahinahmed@users.noreply.github.com>
2019-11-18 03:13:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -167,4 +167,4 @@ def movie(movie_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
