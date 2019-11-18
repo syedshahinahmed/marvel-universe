1ead035bb7e88f2b7c6e645b8c5cc0a28e216ca4
Syed Shahin Ahmed <41708385+syedshahinahmed@users.noreply.github.com>
2019-11-18 03:13:32 +0000

    Update app.py
    Minor Update to work with Heroku

--- app.py
+++ app.py
@@ -158,13 +158,13 @@ def movie(movie_id):
 #####################
 #### Deployment #####
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 
 # Deplayment server
 
-# if __name__ == '__main__':
-#     app.run(host=os.environ.get('IP'),
-#             port=int(os.environ.get('PORT')),
-#             debug=True)
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
